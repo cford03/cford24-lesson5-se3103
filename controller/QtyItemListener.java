ed078232de2f5e1399c4c601452516369dbb6221
Charity <cford24@uco.edu>
2025-02-20 11:21:10 -0600

    south panel set up video 8

---
+++ controller/QtyItemListener.java
@@ -0,0 +1,14 @@
+package controller;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class QtyItemListener implements ItemListener{
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+       
+    }
+
+    
+}
--- view/AppWindow.java
+++ view/AppWindow.java
@@ -2,10 +2,34 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.TitledBorder;
+
+import controller.AddButtonListener;
+import controller.ClearButtonListener;
+import controller.QtyItemListener;
+import controller.RemoveButtonListener;
+import controller.ShapeButtonListener;
 
 public class AppWindow extends JFrame {
+
+
+    public static final String[] shapeActionCommand = 
+        new String[]{"Circle", "Rect", "Stadium"};
+    public static final String addActionCommand = "Add";
+    public static final String removeActionCommand = "Remove";
+    public static final String clearActionCommand = "Clear";
+    public static final Integer[] qtyListItems = new Integer[]{
+        1,2,3,4,5
+    };
 
     private AppCanvas canvas = new AppCanvas();
 
@@ -13,6 +37,58 @@ public class AppWindow extends JFrame {
     public void init() {
         Container cp= getContentPane();
         cp.add(canvas, BorderLayout.CENTER);
+
+        //shape selection
+
+        JPanel shapePanel = new JPanel();
+        shapePanel.setBorder(new TitledBorder("Select Shape"));
+        ButtonGroup shapeGroup = new ButtonGroup();
+        JRadioButton[] shapeButtons = new JRadioButton[]{
+            new JRadioButton(shapeActionCommand[0],true), //default
+            new JRadioButton(shapeActionCommand[1]), 
+            new JRadioButton(shapeActionCommand[2]), 
+        };
+        ShapeButtonListener shapeButtonListener = new ShapeButtonListener();
+        for(var b: shapeButtons){
+            b.addActionListener(shapeButtonListener);
+            shapePanel.add(b);
+            shapeGroup.add(b);
+        }
+        //add panel
+        JPanel addPanel = new JPanel();
+        addPanel.setBorder(new TitledBorder("Add"));
+        JComboBox<Integer>qtyComboBox = new JComboBox<>();
+        for (var q: qtyListItems){
+            qtyComboBox.addItem(q);
+        }
+        qtyComboBox.addItemListener(new QtyItemListener());
+
+        JButton addButton = new JButton(addActionCommand);
+        addButton.addActionListener(new AddButtonListener());
+        addPanel.add(qtyComboBox);
+        addPanel.add(addButton);
+
+        //remove/clear buttons
+        JPanel removeClearPanel = new JPanel();
+        removeClearPanel.setBorder(new TitledBorder("Remove/Clear"));
+        JButton removeButton = new JButton(removeActionCommand);
+        removeButton.addActionListener(new RemoveButtonListener());
+        JButton clearButton = new JButton(clearActionCommand);
+        clearButton.addActionListener(new ClearButtonListener());
+        removeClearPanel.add(removeButton);
+        removeClearPanel.add(clearButton);
+
+
+        //south panel
+
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new GridLayout(3,1));
+        southPanel.add(shapePanel);
+        southPanel.add(addPanel);
+        southPanel.add(removeClearPanel);
+
+        cp.add(southPanel, BorderLayout.SOUTH);
+
 
     }
 }
